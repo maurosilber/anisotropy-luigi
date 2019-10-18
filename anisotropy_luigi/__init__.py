1ddc8c7156265f5fb8f94d214b9d1022889fb4bf
Mauro Silberberg <maurosilber@gmail.com>
2019-10-18 18:42:07 -0300

    Added package config file in __init__.py
    Luigi config files are located either in /etc/luigi or in the current
    working directory. I added some code in __init__.py to include this
    package in Luigi's config files path.
    
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- anisotropy_luigi/__init__.py
+++ anisotropy_luigi/__init__.py
@@ -1,3 +1,10 @@
+from luigi import configuration
+from pkg_resources import resource_filename
+
+config = resource_filename('anisotropy_luigi', 'luigi.cfg')
+configuration.add_config_path(config)
+
 from .files import Files
 from .image import CorrectedImage
 from .tracking import TrackedLabels
+from .main import RunAll
