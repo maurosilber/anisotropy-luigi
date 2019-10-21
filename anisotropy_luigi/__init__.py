e19139456e49fa3d74377a7fbf8b575e6608bd4c
Mauro Silberberg <maurosilber@gmail.com>
2019-10-21 11:58:50 -0300

    Set current working directory as first priority for luigi.cfg
    A luigi.cfg file in the current working directory will override the one
    included with the package.
    
    Luigi config file order is now:
    
    /etc/luigi/luigi.cfg
    luigi.cfg
    <package_install_dir>/anisotropy_luigi/luigi.cfg
    luigi.cfg
    
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- anisotropy_luigi/__init__.py
+++ anisotropy_luigi/__init__.py
@@ -1,8 +1,9 @@
 from luigi import configuration
 from pkg_resources import resource_filename
 
-config = resource_filename('anisotropy_luigi', 'luigi.cfg')
-configuration.add_config_path(config)
+package_config = resource_filename('anisotropy_luigi', 'luigi.cfg')
+configuration.add_config_path(package_config)
+configuration.add_config_path('luigi.cfg')  # Re-adds luigi.cfg as first place to check for a config file
 
 from .files import Files
 from .image import CorrectedImage
