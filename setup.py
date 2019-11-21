f3424ead0be9b58ab1cde3ab534ee6814fdbcece
Mauro Silberberg <maurosilber@gmail.com>
2019-11-21 14:01:56 -0300

    Bugfix: Added package_data in setup.py to incluide config files
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
     name='anisotropy-luigi',
     version='0.0.2',
     packages=['anisotropy_luigi'],
+    package_data={'': ['*.cfg']},
     url='https://github.com/maurosilber/anisotropy-luigi',
     author='Mauro Silberberg',
     author_email='maurosilber@gmail.com',
-    install_requires=['donkey-kong', 'cellment', 'anisotropy', 'pandas', 'parse', 'numpy', 'scipy', 'scikit-image']
+    install_requires=['donkey-kong', 'cellment', 'anisotropy', 'pandas', 'parse', 'numpy',
+                      'scipy', 'scikit-image']
 )
