ee00d42102125f17626577bad97fdfddad4e09e8
Mauro Silberberg <maurosilber@gmail.com>
2019-11-22 10:45:15 -0300

    Capped number of threads used by Intel MKL to 1 in RunAll task
    Signed-off-by: Mauro Silberberg <maurosilber@gmail.com>

--- anisotropy_luigi/main.py
+++ anisotropy_luigi/main.py
@@ -6,5 +6,10 @@ from anisotropy_luigi.cell import AnisotropyJump
 class RunAll(luigi.WrapperTask):
     """Dummy task to preprocess and call all tasks."""
 
+    def __init__(self, *args, **kwargs):
+        import mkl
+        mkl.set_num_threads(1)
+        super().__init__(*args, **kwargs)
+
     def requires(self):
         return AnisotropyJump()
