a6028ee99f8c464ba3b33b2b2b52f338868eff7f
Mauro Silberberg <maurosilber@gmail.com>
2020-06-02 14:05:34 -0300

    Changed normalization in stardist

--- anisotropy_luigi/segmentation.py
+++ anisotropy_luigi/segmentation.py
@@ -3,6 +3,7 @@ import numpy as np
 from donkeykong.target import LocalNpy
 from luigi.util import delegates
 from skimage.segmentation import relabel_sequential
+from cellment.functions import HistogramRV
 
 from .image import CorrectedImage, CorrectedBackground
 from .parameters import CorrectedImageParams
@@ -30,6 +31,10 @@ class Labels(CorrectedImageParams, luigi.Task):
         with self.subtasks()['image'] as ims:
             labels = np.empty(ims.shape, dtype=np.uint16)
             for i, im in enumerate(ims):
-                labels[i] = model.predict_instances(normalize(im.data))[0]
+                bg_rv = HistogramRV(bg_rvs[str(i)])  # loads background distribution
+                v_min, v_int = bg_rv.ppf(0.7), bg_rv.interval(0.5)
+                norm_im = np.clip(im.data, v_min, v_min + 10 * np.diff(v_int))
+                norm_im = (norm_im - norm_im.min()) / norm_im.ptp()
+                labels[i] = model.predict_instances(norm_im)[0]
         dtype = np.min_scalar_type(labels.max())
         self.output().save(labels.astype(dtype, copy=False))
